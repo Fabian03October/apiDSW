52f3ccb6c5f4d4b0530e88699b67f518eb628df9
Marcos7364 <21160618@itoaxaca.edu.mx>
2025-12-13 21:31:04 -0600

    fi4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,6 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'password' => static::$password ??= Hash::make('password'),
             'rol' => 'estudiante',
-            'remember_token' => Str::random(10),
         ];
     }
 
